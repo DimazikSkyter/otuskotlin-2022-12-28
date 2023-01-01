5054fe3bbfa745118636eddf28f54a736c46555b
Дмитрий Цитман <dimazzik@gmail.com>
2023-01-01 23:35:23 +0300

    m1-init: fix all points 1) наличие корневого проекта - в master; 2) наличие подпроектов на Kotlin - m1; 3) правильно настроенный .gitignor и отсутствие мусорных файлов в проекте - m1; 4) версии библиотек в gradle.properties - done in m1.

--- m1/build.gradle.kts
+++ m1/build.gradle.kts
@@ -1,17 +1,5 @@
-plugins {
-    id("java")
-}
+group = "ru.otus.homework1"
 
-group = "ru.otus"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-}
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
