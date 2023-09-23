f7bd2b65dc5da15468999d69469b97bc9f9b320a
Дмитрий Цитман <dimazzik@gmail.com>
2023-09-24 01:19:55 +0300

    m3-api: изменения в предыдущих модулях

--- m1/build.gradle.kts
+++ m1/build.gradle.kts
@@ -1,6 +1,14 @@
 group = "ru.otus.homework1"
 
+plugins {
+    apply { "kotlin" }
+    kotlin("jvm")
+}
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
 }
---
+++ m1/src/test/kotlin/ru/otus/homework1/MainKtTest.kt
@@ -0,0 +1,13 @@
+package ru.otus.homework1
+
+import java.lang.Integer.sum
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class MainKtTest {
+
+    @Test
+    fun shouldSumReturn10From5And5() {
+        assertEquals(/* expected = */ 10, /* actual = */ sum(5,5))
+    }
+}
